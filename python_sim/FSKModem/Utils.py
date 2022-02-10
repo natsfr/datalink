e94f9f9ba3031c98ac7ffaee5f68c0c8c40fa377
F4IHX <f4ihx@rfporn.org>
2022-02-10 10:05:11 +0100

    starting to add basicpolynomial calculation

--- python_sim/FSKModem/Utils.py
+++ python_sim/FSKModem/Utils.py
@@ -11,3 +11,17 @@ import numpy.random as rnd
 
 def CAWGN(power, length):
     return power * np.sqrt(2)/2 *(rnd.randn(int((length))) + 1j * rnd.randn(int(length)));
+
+def polymul(poly0, poly1):
+    a = poly1
+    b = poly0
+    print(a)
+    if(len(poly0) >= len(poly1)):
+        a = poly0
+        b = poly1
+    result = [0] * (len(a)+len(b))
+    for i in np.arange(len(b)): # Shorter Poly
+        for j in np.arange(len(a)): # Longer Poly
+            if a[j] != 0 and b[i] != 0: # X^(i+j) = a[j] * b[i]
+                result[i+j] += a[j] * b[i]
+    return result
---
+++ python_sim/testpoly.py
@@ -0,0 +1,18 @@
+from FSKModem import FSKModem as FMod
+from FSKModem import Utils as FSKUtils
+
+import numpy as np
+import numpy.fft as fft
+ 
+import matplotlib.pyplot as plt
+
+poly0 = [1,0,1,0,1]
+poly1 = [1,1,0,0,1]
+
+print(len(poly0))
+
+poly0.reverse()
+poly1.reverse()
+
+res = FSKUtils.polymul(poly0, poly1)
+res.reverse()
